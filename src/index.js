be768729d29e5095735784cdf6eec538128d2b66
Rois Faozi <67546196+Roisfaozi@users.noreply.github.com>
2020-08-18 21:21:49 +0700

    first commit adding webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import $ from "jquery";
+import moment from "moment";
+import 'regenerator-runtime';
+import "./script/component/app-bar.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
