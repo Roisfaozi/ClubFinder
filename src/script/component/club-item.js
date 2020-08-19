8784d9f4b38b5d72c3d3f7b04c89a710a152c225
Roisfaozi <roisfaozi55@gmail.com>
2020-08-19 23:44:52 +0700

    menambahkan Fetch API ke thesportdb

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,16 +1,16 @@
-class ClubItem extends HTMLElement{
+class ClubItem extends HTMLElement {
 
     constructor() {
         super();
-        this.shadowDOM = this.attachShadow({mode: "open"});
+        this.shadowDOM = this.attachShadow({ mode: "open" });
     }
 
     set club(club) {
-        this._club =club;
+        this._club = club;
         this.render();
     }
 
-    render(){
+    render() {
         this.shadowDOM.innerHTML = `
         <style>
 
@@ -52,10 +52,10 @@ class ClubItem extends HTMLElement{
             -webkit-line-clamp: 10; /* number of lines to show */
         }
         </style>
-        <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+        <img class="fan-art-club" src="${this._club.strTeamBadge}" alt="Fan Art">
         <div class="club-info">
-        <h2>${this._club.name}</h2>
-        <p>${this._club.description}</p>
+        <h2>${this._club.strTeam}</h2>
+        <p>${this._club.strDescriptionEN}</p>
         </div>`;
     }
 }
