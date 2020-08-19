dffbede55b69e1c3ceb6562a61cdd564c92f1ce5
Roisfaozi <roisfaozi55@gmail.com>
2020-08-19 12:50:55 +0700

    menambahkan loader, plugins, webpack-dev-server dan webpack-merge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-
-import $ from "jquery";
-import moment from "moment";
-import 'regenerator-runtime';
+import "./styles/style.css";
+import "regenerator-runtime";
 import "./script/component/app-bar.js";
 import main from "./script/view/main.js";
 
